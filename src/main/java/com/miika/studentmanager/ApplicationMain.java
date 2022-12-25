b42a5f77c294e60b165a8503c9659d038fe79f7e
miika <miika.karjalainen13@gmail.com>
2022-12-25 19:53:43 +0200

    login works.

--- src/main/java/com/miika/studentmanager/ApplicationMain.java
+++ src/main/java/com/miika/studentmanager/ApplicationMain.java
@@ -1,11 +1,22 @@
 package com.miika.studentmanager;
 
+import org.hibernate.annotations.common.util.impl.Log;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.swing.*;
+import java.sql.*;
+import java.util.List;
 
 public class ApplicationMain {
+
+    private Connection conn;
 
     public static void main(String[] args) {
         // TODO: login system
@@ -15,23 +26,23 @@ public class ApplicationMain {
 
     }
 
-    public boolean auth(String username, String password) {
-        Login l = new Login();
-        l.setUsername(username);
-        l.setPassword(password);
+    public void auth(String username, String password) throws SQLException {
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/students_schema", "root", "password");
 
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("JPA-PU");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM login");
 
-        TypedQuery<Login> query = entityManager.createQuery("SELECT l FROM Login l WHERE l.username = :username",
-                Login.class);
-        query.setParameter("username", username);
-
-        Login login = query.getSingleResult();
-        if (login == null) {
-            return false;
+        while (rs.next()) {
+            String dbUsername = rs.getString("username");
+            String dbPassword = rs.getString("password");
+            if(dbUsername.contentEquals(username) && dbPassword.contentEquals(password)) {
+                GraphicalSwing n = new GraphicalSwing();
+            } else {
+                ErrorDialog e = new ErrorDialog("Username or password is not correct.");
+            }
         }
+        conn.close();
 
-        return true;
     }
 }
--- src/main/java/com/miika/studentmanager/LoginSwing.java
+++ src/main/java/com/miika/studentmanager/LoginSwing.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.SQLException;
 
 public class LoginSwing {
     private JFrame mainFrame;
@@ -51,7 +52,11 @@ public class LoginSwing {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ApplicationMain a = new ApplicationMain();
-                a.auth(usernameTxt.getText(), String.valueOf(passwordTxt.getPassword()));
+                try {
+                    a.auth(usernameTxt.getText(), String.valueOf(passwordTxt.getPassword()));
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
 
@@ -60,7 +65,7 @@ public class LoginSwing {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ApplicationMain a = new ApplicationMain();
-                a.auth(usernameTxt.getText(), String.valueOf(passwordTxt.getPassword()));
+                //a.auth(usernameTxt.getText(), String.valueOf(passwordTxt.getPassword()));
             }
         });
 
